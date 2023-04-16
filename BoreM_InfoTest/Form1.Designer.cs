8d97cb2a246a1c7427a65548fe2d74d59b93b976
marcoborelli <marco05.borelli@gmail.com>
2023-04-16 22:17:55 +0200

    Implementazione versione grafica

--- BoreM_InfoTest/Form1.Designer.cs
+++ BoreM_InfoTest/Form1.Designer.cs
@@ -23,20 +23,176 @@
         /// il contenuto del metodo con l'editor di codice.
         /// </summary>
         private void InitializeComponent() {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonAggiuntaCan = new System.Windows.Forms.Button();
+            this.labelNMatricola = new System.Windows.Forms.Label();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.textBoxNMatricola = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.comboBoxStato = new System.Windows.Forms.ComboBox();
+            this.labelStatLav = new System.Windows.Forms.Label();
+            this.textBoxBOH1 = new System.Windows.Forms.TextBox();
+            this.labelBOH1 = new System.Windows.Forms.Label();
+            this.labelBOH2 = new System.Windows.Forms.Label();
+            this.comboBoxBOH2 = new System.Windows.Forms.ComboBox();
+            this.buttonRimuoviCan = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(444, 358);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseDoubleClick);
+            // 
+            // buttonAggiuntaCan
+            // 
+            this.buttonAggiuntaCan.Location = new System.Drawing.Point(600, 316);
+            this.buttonAggiuntaCan.Name = "buttonAggiuntaCan";
+            this.buttonAggiuntaCan.Size = new System.Drawing.Size(100, 54);
+            this.buttonAggiuntaCan.TabIndex = 1;
+            this.buttonAggiuntaCan.Text = "AGGIUNGI CANDIDATO";
+            this.buttonAggiuntaCan.UseVisualStyleBackColor = true;
+            this.buttonAggiuntaCan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelNMatricola
+            // 
+            this.labelNMatricola.AutoSize = true;
+            this.labelNMatricola.Location = new System.Drawing.Point(491, 26);
+            this.labelNMatricola.Name = "labelNMatricola";
+            this.labelNMatricola.Size = new System.Drawing.Size(100, 13);
+            this.labelNMatricola.TabIndex = 2;
+            this.labelNMatricola.Text = "NUM. MATRICOLA";
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(552, 52);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(39, 13);
+            this.labelNome.TabIndex = 3;
+            this.labelNome.Text = "NOME";
+            // 
+            // textBoxNMatricola
+            // 
+            this.textBoxNMatricola.Location = new System.Drawing.Point(600, 19);
+            this.textBoxNMatricola.Name = "textBoxNMatricola";
+            this.textBoxNMatricola.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNMatricola.TabIndex = 4;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(600, 45);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNome.TabIndex = 5;
+            // 
+            // comboBoxStato
+            // 
+            this.comboBoxStato.FormattingEnabled = true;
+            this.comboBoxStato.Location = new System.Drawing.Point(600, 184);
+            this.comboBoxStato.Name = "comboBoxStato";
+            this.comboBoxStato.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxStato.TabIndex = 6;
+            this.comboBoxStato.SelectedIndexChanged += new System.EventHandler(this.comboBoxStato_SelectedIndexChanged);
+            // 
+            // labelStatLav
+            // 
+            this.labelStatLav.AutoSize = true;
+            this.labelStatLav.Location = new System.Drawing.Point(480, 192);
+            this.labelStatLav.Name = "labelStatLav";
+            this.labelStatLav.Size = new System.Drawing.Size(114, 13);
+            this.labelStatLav.TabIndex = 7;
+            this.labelStatLav.Text = "STATO LAVORATIVO";
+            // 
+            // textBoxBOH1
+            // 
+            this.textBoxBOH1.Location = new System.Drawing.Point(600, 71);
+            this.textBoxBOH1.Name = "textBoxBOH1";
+            this.textBoxBOH1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBOH1.TabIndex = 8;
+            // 
+            // labelBOH1
+            // 
+            this.labelBOH1.AutoSize = true;
+            this.labelBOH1.Location = new System.Drawing.Point(462, 78);
+            this.labelBOH1.Name = "labelBOH1";
+            this.labelBOH1.Size = new System.Drawing.Size(35, 13);
+            this.labelBOH1.TabIndex = 10;
+            this.labelBOH1.Text = "label1";
+            // 
+            // labelBOH2
+            // 
+            this.labelBOH2.AutoSize = true;
+            this.labelBOH2.Location = new System.Drawing.Point(556, 105);
+            this.labelBOH2.Name = "labelBOH2";
+            this.labelBOH2.Size = new System.Drawing.Size(35, 13);
+            this.labelBOH2.TabIndex = 11;
+            this.labelBOH2.Text = "label1";
+            // 
+            // comboBoxBOH2
+            // 
+            this.comboBoxBOH2.FormattingEnabled = true;
+            this.comboBoxBOH2.Location = new System.Drawing.Point(600, 97);
+            this.comboBoxBOH2.Name = "comboBoxBOH2";
+            this.comboBoxBOH2.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxBOH2.TabIndex = 12;
+            // 
+            // buttonRimuoviCan
+            // 
+            this.buttonRimuoviCan.Location = new System.Drawing.Point(483, 316);
+            this.buttonRimuoviCan.Name = "buttonRimuoviCan";
+            this.buttonRimuoviCan.Size = new System.Drawing.Size(100, 54);
+            this.buttonRimuoviCan.TabIndex = 13;
+            this.buttonRimuoviCan.Text = "RIMUOVI CANDIDATO";
+            this.buttonRimuoviCan.UseVisualStyleBackColor = true;
+            this.buttonRimuoviCan.Click += new System.EventHandler(this.buttonRimuoviCan_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(729, 450);
+            this.Controls.Add(this.buttonRimuoviCan);
+            this.Controls.Add(this.comboBoxBOH2);
+            this.Controls.Add(this.labelBOH2);
+            this.Controls.Add(this.labelBOH1);
+            this.Controls.Add(this.textBoxBOH1);
+            this.Controls.Add(this.labelStatLav);
+            this.Controls.Add(this.comboBoxStato);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxNMatricola);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.labelNMatricola);
+            this.Controls.Add(this.buttonAggiuntaCan);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonAggiuntaCan;
+        private System.Windows.Forms.Label labelNMatricola;
+        private System.Windows.Forms.Label labelNome;
+        private System.Windows.Forms.TextBox textBoxNMatricola;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.ComboBox comboBoxStato;
+        private System.Windows.Forms.Label labelStatLav;
+        private System.Windows.Forms.TextBox textBoxBOH1;
+        private System.Windows.Forms.Label labelBOH1;
+        private System.Windows.Forms.Label labelBOH2;
+        private System.Windows.Forms.ComboBox comboBoxBOH2;
+        private System.Windows.Forms.Button buttonRimuoviCan;
     }
 }
 
--- BoreM_InfoTest/Form1.cs
+++ BoreM_InfoTest/Form1.cs
@@ -11,14 +11,136 @@ using System.Windows.Forms;
 namespace BoreM_InfoTest {
     public partial class Form1 : Form {
         List<BoreM_Candidato> candidati;/*non uso dizionario perchè la chiave è il nome ma non ho un valore vero da associare poi*/
+        string[] statiCandidato = new string[] { "Disoccupato", "Lavoratore" };
+        string[] lode = new string[] { "True", "False" };
+        string[] testiButton = new string[] { "AGGIUNGI CANDIDATO", "AGGIORNA CANDIDATO" };
+        bool aggiungi = true;
         public Form1() {
             candidati = new List<BoreM_Candidato>();
             InitializeComponent();
         }
         private void Form1_Load(object sender, EventArgs e) {
+            dataGridView1.Columns.Add("nmatricola", "NUM MATRICOLA");
+            dataGridView1.Columns.Add("nome", "NOME");
+            dataGridView1.Columns.Add("extra1", "EXTRA 1");
+            dataGridView1.Columns.Add("extra2", "EXTRA 2");
 
+            comboBoxStato.DropDownStyle = comboBoxBOH2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxStato.Items.AddRange(statiCandidato);
+            comboBoxBOH2.Items.AddRange(lode);
+
+            comboBoxStato.Text = statiCandidato[0];
+            comboBoxBOH2.Text = lode[0];
+
+            labelBOH1.Visible = labelBOH2.Visible = textBoxBOH1.Visible = comboBoxBOH2.Visible = false;
+            comboBoxStato_SelectedIndexChanged(sender, e);
         }
 
+        private void button1_Click(object sender, EventArgs e) {/*aggiunta candidato*/
+            if (!textBoxNMatricola.Text.All(char.IsDigit) || !textBoxBOH1.Text.All(char.IsDigit)) {
+                string tmp;
+
+                if (comboBoxStato.SelectedIndex == 0)
+                    tmp = "voto di uscita";
+                else
+                    tmp = "numero di lavori";
+
+                MessageBox.Show($"Inserire come matricola e come {tmp} un numero");
+                return;
+            }
+
+            if (comboBoxStato.SelectedIndex == 0) {
+                BoreM_Disoccupato tmp = new BoreM_Disoccupato(int.Parse(textBoxNMatricola.Text), textBoxNome.Text, byte.Parse(textBoxBOH1.Text), bool.Parse(comboBoxBOH2.Text));
+                if (aggiungi)
+                    AggiuntaCandidato(tmp);
+                else
+                    ModificaCandidato(tmp);
+            } else if (comboBoxStato.SelectedIndex == 1) {
+                BoreM_Lavoratore tmp = new BoreM_Lavoratore(int.Parse(textBoxNMatricola.Text), textBoxNome.Text, byte.Parse(textBoxBOH1.Text));
+                if (aggiungi)
+                    AggiuntaCandidato(tmp);
+                else
+                    ModificaCandidato(tmp);
+            }
+
+            aggiungi = true;
+            RefreshDataGridView();
+            ResetTutto();
+        }
+        private void comboBoxStato_SelectedIndexChanged(object sender, EventArgs e) {
+            CambiaGrafica(comboBoxStato.SelectedIndex);
+            ResetTutto();
+        }
+        private void dataGridView1_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e) {
+            if (dataGridView1.SelectedRows[0].IsNewRow) {
+                ResetTutto();
+                return;
+            }
+
+            aggiungi = false;
+            if (dataGridView1.SelectedRows[0].Cells[3].Value.ToString() != "") {
+                comboBoxStato.Text = statiCandidato[0];
+                comboBoxBOH2.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            } else {
+                comboBoxStato.Text = statiCandidato[1];
+            }
+
+            buttonAggiuntaCan.Text = testiButton[1];
+
+            textBoxNMatricola.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBoxNome.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textBoxBOH1.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+        }
+        private void buttonRimuoviCan_Click(object sender, EventArgs e) {/*rimuovi candidato*/
+            if (dataGridView1.SelectedRows[0].IsNewRow) {
+                return;
+            }
+
+            if (dataGridView1.SelectedRows[0].Cells[3].Value.ToString() != "") {
+                BoreM_Disoccupato tmp = new BoreM_Disoccupato(int.Parse(dataGridView1.SelectedRows[0].Cells[0].Value.ToString()), dataGridView1.SelectedRows[0].Cells[1].Value.ToString(), byte.Parse(dataGridView1.SelectedRows[0].Cells[2].Value.ToString()), bool.Parse(dataGridView1.SelectedRows[0].Cells[2].Value.ToString()));
+                RimuoviCandidato(tmp);
+            } else {
+                BoreM_Lavoratore tmp = new BoreM_Lavoratore(int.Parse(dataGridView1.SelectedRows[0].Cells[0].Value.ToString()), dataGridView1.SelectedRows[0].Cells[1].Value.ToString(), byte.Parse(dataGridView1.SelectedRows[0].Cells[2].Value.ToString()));
+                RimuoviCandidato(tmp);
+            }
+            RefreshDataGridView();
+        }
+
+        public void CambiaGrafica(int scelta) {
+            if (scelta == 0) {
+                labelBOH1.Text = "VOTO";
+                labelBOH2.Text = "LODE";
+                labelBOH1.Visible = labelBOH2.Visible = textBoxBOH1.Visible = comboBoxBOH2.Visible = true;
+            } else {
+                labelBOH1.Text = "NUM DI ESPERIENZE [5]";
+                labelBOH1.Visible = textBoxBOH1.Visible = true;
+                labelBOH2.Visible = comboBoxBOH2.Visible = false;/*resetto le altre 2*/
+            }
+        }
+        public void ResetTutto() {
+            textBoxBOH1.Text = textBoxNMatricola.Text = textBoxNome.Text = "";
+            buttonAggiuntaCan.Text = testiButton[0];
+        }
+
+        public void RefreshDataGridView() {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+            string[] tmp = RitornaElementiPerDataGridView();
+
+            for (int i = 0; i < tmp.Length; i++) {
+                string[] tt = tmp[i].Split(';');
+                if (tt.Length == 3) {
+                    string[] ttt = new string[4];
+                    for (int j = 0; j < tt.Length; j++) {
+                        ttt[j] = tt[j];
+                    }
+                    ttt[tt.Length] = "";
+                    dataGridView1.Rows.Add(ttt);
+                } else
+                    dataGridView1.Rows.Add(tt);
+            }
+        }
         public bool AggiuntaCandidato(BoreM_Candidato c) {
             if (candidati.Contains(c) || c == null) {
                 return false;
@@ -61,7 +183,8 @@ namespace BoreM_InfoTest {
         }
 
         public void OrdinaDecrescente() {
-            candidati.OrderByDescending<>;
+            candidati.Sort();
+            candidati.Reverse();
         }
 
         public string[] RitornaIdonei() {
@@ -76,7 +199,6 @@ namespace BoreM_InfoTest {
                         lav.Add(candidati[i].ToString());
                     else if (candidati[i].GetType() == typeof(BoreM_Disoccupato))
                         disoc.Add(candidati[i].ToString());
-
                 }
             }
 
